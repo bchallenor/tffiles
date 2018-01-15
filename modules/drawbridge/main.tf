fce3c36b0fec66cfed3079a3956fe2be07bc99cd
Ben Challenor <ben@challenor.org>
2018-01-15 05:31:22 +0000

    Define egress, as Drawbridge only manages ingress

--- modules/drawbridge/main.tf
+++ modules/drawbridge/main.tf
@@ -15,4 +15,12 @@ resource "aws_security_group" "sg" {
     Name       = "${local.name}"
     Drawbridge = "${var.profile}"
   }
+
+  # Drawbridge controls ingress only
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
