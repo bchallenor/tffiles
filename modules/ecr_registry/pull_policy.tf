53bceeee9b602d1542ae421aca59f0216f181eb3
Ben Challenor <ben@challenor.org>
2018-09-10 20:41:27 +0100

    Create ECR registry module

---
+++ modules/ecr_registry/pull_policy.tf
@@ -0,0 +1,21 @@
+data "aws_iam_policy_document" "pull_policy" {
+  statement {
+    actions = [
+      "ecr:GetAuthorizationToken",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+    ]
+
+    resources = [
+      "${aws_ecr_repository.repo.*.arn}",
+    ]
+  }
+}
---
+++ modules/ecr_registry/push_policy.tf
@@ -0,0 +1,25 @@
+data "aws_iam_policy_document" "push_policy" {
+  statement {
+    actions = [
+      "ecr:GetAuthorizationToken",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:PutImage",
+    ]
+
+    resources = [
+      "${aws_ecr_repository.repo.*.arn}",
+    ]
+  }
+}
