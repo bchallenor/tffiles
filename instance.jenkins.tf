76c96fd1608eb9626ae92ff7ca1d4729ebde4782
Ben Challenor <ben@challenor.org>
2018-09-15 00:32:19 +0100

    Extract jenkins instance to file

---
+++ instance.jenkins.tf
@@ -0,0 +1,43 @@
+module "jenkins_secrets_volume" {
+  source            = "./modules/volume"
+  name              = "jenkins-secrets"
+  size              = 1
+  availability_zone = "${var.stable_availability_zone}"
+
+  providers = {
+    "aws" = "aws.stable"
+  }
+}
+
+module "jenkins_volume" {
+  source            = "./modules/volume"
+  name              = "jenkins"
+  size              = 4
+  availability_zone = "${var.stable_availability_zone}"
+
+  providers = {
+    "aws" = "aws.stable"
+  }
+}
+
+module "jenkins_instance" {
+  source                = "./modules/spot_instance"
+  name                  = "jenkins"
+  instance_type         = "t3.medium"
+  spot_price            = "0.018"
+  availability_zone     = "${var.stable_availability_zone}"
+  instance_profile_name = "${module.jenkins_profile.profile_name}"
+  root_volume_size      = 20
+  security_group_id     = "${module.drawbridge_test.security_group_id}"
+  zone_id               = "${module.cloud_zone.id}"
+  zone_name             = "${module.cloud_zone.name}"
+
+  persistent_volume_ids = [
+    "${module.jenkins_secrets_volume.id}",
+    "${module.jenkins_volume.id}",
+  ]
+
+  providers = {
+    "aws" = "aws.stable"
+  }
+}
