cec8e129291412aee548c784f681198d09314206
Ben Challenor <ben@challenor.org>
2018-09-21 17:39:32 +0100

    Prefix drawbridge security group names

--- modules/drawbridge/main.tf
+++ modules/drawbridge/main.tf
@@ -1,12 +1,12 @@
 provider "aws" {}
 
 resource "aws_security_group" "sg" {
-  name        = "${var.name}"
+  name        = "drawbridge-${var.name}"
   description = "Managed by Drawbridge"
   vpc_id      = "${var.vpc_id}"
 
   tags {
-    Name       = "${var.name}"
+    Name       = "drawbridge-${var.name}"
     Drawbridge = "${var.name}"
   }
 
