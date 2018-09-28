bc161bfb099ba676bad6a5f399de08dfdbed404d
Ben Challenor <ben@challenor.org>
2018-09-28 14:43:33 +0100

    Define role assumption in user module

--- modules/user/main.tf
+++ modules/user/main.tf
@@ -3,6 +3,20 @@ resource "aws_iam_user" "user" {
   force_destroy = "true"
 }
 
+resource "aws_iam_user_policy" "assume_role" {
+  name   = "assume-role"
+  user   = "${aws_iam_user.user.name}"
+  policy = "${data.aws_iam_policy_document.assume_role.json}"
+  count  = "${length(var.role_arns) > 0 ? 1 : 0}"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions   = ["sts:AssumeRole"]
+    resources = ["${var.role_arns}"]
+  }
+}
+
 resource "aws_iam_user_policy_attachment" "user" {
   user       = "${aws_iam_user.user.name}"
   policy_arn = "${element(var.policy_arns, count.index)}"
