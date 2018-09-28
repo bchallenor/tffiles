ae987d33f66a633f45deb3a6a47c912f76b8613b
Ben Challenor <ben@challenor.org>
2018-09-28 13:54:39 +0100

    Optionally create instance profile

--- modules/role/main.tf
+++ modules/role/main.tf
@@ -15,3 +15,9 @@ resource "aws_iam_role_policy_attachment" "role" {
   policy_arn = "${element(var.policy_arns, count.index)}"
   count      = "${length(var.policy_arns)}"
 }
+
+resource "aws_iam_instance_profile" "profile" {
+  name  = "${var.name}"
+  role  = "${aws_iam_role.role.name}"
+  count = "${var.create_instance_profile ? 1 : 0}"
+}
--- modules/role/trust_policy.tf
+++ modules/role/trust_policy.tf
@@ -15,13 +15,18 @@ data "aws_iam_policy_document" "mfa_trust_policy" {
   }
 }
 
+locals {
+  instance_profile_trusted_services = "${slice(list("ec2.amazonaws.com"), 0, var.create_instance_profile ? 1 : 0)}"
+  trusted_services                  = "${distinct(concat(var.trusted_services, local.instance_profile_trusted_services))}"
+}
+
 data "aws_iam_policy_document" "mfa_and_services_trust_policy" {
   source_json = "${data.aws_iam_policy_document.mfa_trust_policy.json}"
 
   statement {
     principals {
       type        = "Service"
-      identifiers = ["${var.trusted_services}"]
+      identifiers = ["${local.trusted_services}"]
     }
 
     actions = ["sts:AssumeRole"]
@@ -29,7 +34,7 @@ data "aws_iam_policy_document" "mfa_and_services_trust_policy" {
 }
 
 locals {
-  trust_policy_json = "${length(var.trusted_services) > 0 ? data.aws_iam_policy_document.mfa_and_services_trust_policy.json : data.aws_iam_policy_document.mfa_trust_policy.json}"
+  trust_policy_json = "${length(local.trusted_services) > 0 ? data.aws_iam_policy_document.mfa_and_services_trust_policy.json : data.aws_iam_policy_document.mfa_trust_policy.json}"
 }
 
 data "aws_caller_identity" "self" {}
