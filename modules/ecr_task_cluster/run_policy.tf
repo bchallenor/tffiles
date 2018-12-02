6cc922bbed7c62be5fec4b48d6e48add40a89457
Ben Challenor <ben@challenor.org>
2018-12-02 17:19:52 +0000

    Grant describe permissions in task run policy

--- modules/ecr_task_cluster/run_policy.tf
+++ modules/ecr_task_cluster/run_policy.tf
@@ -1,12 +1,37 @@
 data "aws_iam_policy_document" "run_policy" {
   statement {
     actions = [
+      "ecs:DescribeTaskDefinition",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
       "ecs:RunTask",
     ]
 
     resources = [
       "${aws_ecs_task_definition.task.*.arn}",
     ]
+  }
+
+  statement {
+    actions = [
+      "ecs:DescribeTasks",
+    ]
+
+    resources = ["*"]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "ecs:cluster"
+
+      values = [
+        "${aws_ecs_cluster.default.arn}",
+      ]
+    }
   }
 
   statement {
